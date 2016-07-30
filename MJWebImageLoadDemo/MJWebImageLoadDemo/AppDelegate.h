68fab7bc128bdd25dc55e1fde822b675e6b14af2
KimiChenMegJie <kimi@kimideiMac.local>
2016-07-30 21:06:00 +0800

    提交基础文件

---
+++ MJWebImageLoadDemo/MJWebImageLoadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJWebImageLoadDemo
+//
+//  Created by kimi on 16/7/30.
+//  Copyright © 2016年 kimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJWebImageLoadDemo/MJWebImageLoadDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJWebImageLoadDemo
+//
+//  Created by kimi on 16/7/30.
+//  Copyright © 2016年 kimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
